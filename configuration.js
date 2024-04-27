4f3d5601f360aa6dcf7a9beb1ded2ad89751354c
sinedon <111921789+sinedon@users.noreply.github.com>
2024-04-26 21:33:42 -0400

    Update configuration.js

--- configuration.js
+++ configuration.js
@@ -1,7 +1,7 @@
 const mode = 1;
 
 const host_local = "http://localhost:8080";
-const host_remote = "https://coffee-services-latest.onrender.com";
+const host_remote = "https://coffee-serviceshw9-latest.onrender.com";
 
 function getHost() {
     return (mode === 0) ? host_local : host_remote;
